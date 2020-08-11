3c843b58efc4e987dd3dafe6d2b2fcdbd85633ea
Tiago Freitag <freitagtiago@gmail.com>
2020-08-10 22:34:50 -0300

    upd
    utility assets

---
+++ Galaxya/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadFirstScene",3f);
+    }
+
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
