b72090b80a69ccb0d7ce37fd15e29cfea1bd9062
Tiago Freitag <freitagtiago@gmail.com>
2020-08-11 22:04:55 -0300

    upd

---
+++ Galaxya/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 4f;
+    [SerializeField] float xLimit = 5f;
+    [SerializeField] float yLimit = 3.5f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -30f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
+    float xThrow;
+    float yThrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        transform.localPosition = GetXPosition();
+        transform.localPosition = GetYPosition();
+    }
+
+    private Vector3 GetXPosition()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float clampedXPos = Mathf.Clamp(transform.localPosition.x + xOffset, xLimit * -1, xLimit);
+        return new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    private Vector3 GetYPosition()
+    {
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime;
+        float clampedYPos = Mathf.Clamp(transform.localPosition.y + yOffset, yLimit * -1, yLimit);
+        return new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
+}
